b7ea8fd1721230577986343a2372bea1fef394c3
Joseph ASSIGA <joseph.assiga@gmail.com>
2016-05-22 00:07:55 +0200

    docker

--- app/identidock.py
+++ app/identidock.py
@@ -1,9 +1,17 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, Response
+import requests
 
-@app.route('/')
-def hello_world():
-    return 'Hello World Joseph on Docker!\n'
+app = Flask(__name__)
+default_name = 'Joe Docker'
+
+@app.route('/')def get_identicon():
+        name = default_name        header = '<html><head><title>Identidock</title></head><body>'        body = '''<form method="POST">                  Hello <input type="text" name="name" value="{}">                  <input type="submit" value="submit">                  </form>                  <p>You look like a:                  <img src="/monster/monster.png"/>                  '''.format(name)        footer = '</body></html>'        return header + body + footer
+
+@app.route('/monster/<name>')
+def get_identicon(name):
+    r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+    image = r.content
+    return Response(image, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+        app.run(debug=True, host='0.0.0.0')
